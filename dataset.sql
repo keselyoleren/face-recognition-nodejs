65f3337128af4a3b01dfc4273470a8ed142d6045
Ardy <ardy@adiwidya.co.id>
2018-11-19 10:13:57 +0700

    tabel dataset

---
+++ dataset.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 19, 2018 at 03:54 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `neuravision`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dataset`
+--
+
+CREATE TABLE IF NOT EXISTS `dataset` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dataset_name` varchar(100) NOT NULL,
+  `dataset_action` varchar(30) NOT NULL,
+  `dataset_image_location` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
